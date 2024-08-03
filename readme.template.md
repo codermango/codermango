8e5d8e6b4866f0c9e400e1726e88c3005927da30
Mark <codermango@gmail.com>
2024-08-03 11:14:13 +0200

    v2 (#11)

--- readme.template.md
+++ readme.template.md
@@ -69,9 +69,4 @@ Here are the technologies I have used and am currently using:
 
 **Recent Stars...**
 
-- [hehuapei/visitor-badge](https://github.com/hehuapei/visitor-badge) A badge generator service to count visitors of your markdown file. Fork From:jwenjian/visitor-badge
-- [anuraghazra/github-readme-stats](https://github.com/anuraghazra/github-readme-stats) :zap: Dynamically generated stats for your github readmes
-- [wfp/designsystem](https://github.com/wfp/designsystem) World Food Programme User Interface Kit. Developer kit for implementing WFP web style guideline.
-- [sunface/rust-course](https://github.com/sunface/rust-course) “连续六年成为全世界最受喜爱的语言，无 GC 也无需手动内存管理、极高的性能和安全性、过程/OO/函数式编程、优秀的包管理、JS 未来基石" — 工作之余的第二语言来试试 Rust 吧。<<Rust语言圣经>>拥有全面且深入的讲解、生动贴切的示例、德芙般丝滑的内容，甚至还有JS程序员关注的 WASM 和 Deno 等专题。这可能是目前最用心的 Rust 中文学习教程 / Book 
-- [microsoft/cascadia-code](https://github.com/microsoft/cascadia-code) This is a fun, new monospaced font that includes programming ligatures and is designed to enhance the modern look and feel of the Windows Terminal.
-
+<!-- recent_star_inject -->
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { readFile, rm, writeFile } from "fs/promises";
+import axios from "axios";
+
+const githubAPIEndPoint = "https://api.github.com";
+
+const gh = axios.create({
+  baseURL: githubAPIEndPoint,
+  timeout: 4000,
+});
+
+async function main() {
+  console.log("Start generating README.md...");
+
+  try {
+    const readmeContent = await readFile("./readme.template.md", {
+      encoding: "utf-8",
+    });
+
+    let updatedReadmeContent = readmeContent;
+
+    const star: any[] = await gh
+      .get("/users/codermango/starred")
+      .then((data) => data.data);
+
+    // get top5 recent starred repos
+    const top5Starred = star
+      .sort(
+        (a, b) =>
+          new Date(b.starred_at).getTime() - new Date(a.starred_at).getTime()
+      )
+      .slice(0, 5);
+    let top5StarredMDText = "";
+    top5Starred.forEach((repo) => {
+      top5StarredMDText += `- [${repo.full_name}](${repo.html_url}) ${repo.description}\n`;
+    });
+
+    updatedReadmeContent = updatedReadmeContent.replace(
+      "<!-- recent_star_inject -->",
+      top5StarredMDText
+    );
+
+    // before remove existing README.md, check if it exists
+    try {
+      await readFile("readme.md", { encoding: "utf-8" });
+      await rm("readme.md");
+    } catch (e) {}
+
+    // generate README.md
+    await writeFile("readme.md", updatedReadmeContent, { encoding: "utf-8" });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(`README.md generated successfully at ${new Date()}`);
+}
+
+main();
